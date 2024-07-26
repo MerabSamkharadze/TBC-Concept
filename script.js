1c2dd9bf139dfa57f1cf73cc6c5c8472641f04dd
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-07-26 21:32:26 +0400

    add triger

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
     menuButton.classList.toggle("active");
   });
 });
+
+/// triger
+const triger_icon_over = document.querySelector(".triger_icon_over");
+const triger_icon = document.querySelector(".triger_icon");
+const button_menu_trigger = document.querySelector(".button_menu_trigger");
+const button_clicked_content = document.querySelector(
+  ".button_clicked_content"
+);
+const button_menu = document.querySelector(".button_menu");
+let clicked = true;
+button_menu_trigger.addEventListener("click", () => {
+  if (clicked) {
+    button_clicked_content.style.opacity = "1";
+    triger_icon.style.opacity = "0";
+    triger_icon_over.style.opacity = "1";
+    button_menu.style.gridTemplateRows = "1fr 56px";
+
+    clicked = false;
+  } else {
+    button_clicked_content.style.opacity = "0";
+    triger_icon_over.style.opacity = "0";
+    triger_icon.style.opacity = "1";
+    button_menu.style.gridTemplateRows = "56px";
+
+    clicked = true;
+  }
+});
